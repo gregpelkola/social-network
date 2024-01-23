0e63605f2f70c9953cbd397f6f38c5cc0018b27a
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-01-22 21:38:09 -0600

    user-routes.js, set route endpoints for getAllUsers, getUserById, add and delete friend

---
+++ routes/api-routes/user-rotues.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUserById,
+  deleteUserById,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/user-controller');
+
+// GET and POST all users
+router.route('/').get(getAllUsers).post(createUser);
+
+// GET user id, PUT update user id and DELETE user by id
+router.route('/:userId').get(getUserById).put(updateUserById).delete(deleteUserById);
+
+// POST add friend and DELETE remove Friend
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
