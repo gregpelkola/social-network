910eabc47a8b56168f1339db2be713a5f715b3d3
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-01-23 19:21:26 -0600

    npm init, user-controller fixed and replaced, updated file name due to typo

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-
+console.log('Index.js: Before requiring user-routes and thought-routes');
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
+console.log('Index.js: After requiring user-routes and thought-routes');
 // Define endpoints for user and thought routes
 router.use('/user',userRoutes);
 router.use('/thought',thoughtRoutes);
